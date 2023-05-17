69e12d3f4bcc75b1e32779e37a412084dc878bb0
Binderiya Tsogt <binderiya@Binderiyas-MacBook-Air.local>
2023-05-18 00:33:36 +0800

    first commit

---
+++ src/components/FixedSideBar.tsx
@@ -0,0 +1,74 @@
+import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import profile from "../assets/profile.jpg";
+
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineMail,
+} from "react-icons/ai";
+
+const FixedSideBar: FC = () => (
+  <div className="fixed left-[20px] top-[90px] rounded-[30px] border border-color-3 p-[30px] max-w-[350px] text-white flex flex-col">
+    <header className="flex items-center justify-between mb-[30px]">
+      <h1 className="font-[600] text-[30px]">Binderiya</h1>
+      <span className="text-sm-regular text-end">
+        Framer Designer
+        <br /> & Developer
+      </span>
+    </header>
+    <div className="w-[250px] h-[210px] overflow-hidden mx-[10px] mb-[46px] rounded-[30px]">
+      <Image
+        src={profile}
+        alt=""
+        width={500}
+        height={420}
+        className="w-full h-full object-cover"
+      />
+    </div>
+    <h2 className="text-2xl-slim mb-2 text-center">hello@binderiya</h2>
+    <h2 className="text-2xl-slim text-center mb-[30px]">
+      Base in Ulaanbaatar, MN
+    </h2>
+    <p className="text-sm-regular2 text-color-2 mb-[25px] text-center">
+      © 2022 Binderiya. All Rights Reserved
+    </p>
+    <div className="flex items-center justify-center flex-wrap gap-2 mb-[30px]">
+      <Link
+        href={`https://twitter.com`}
+        className="text-color-2 border border-color-4 rounded-full w-[50px] h-[50px] grid place-items-center hover:text-color-1 hover:border hover:border-color-1 duration-300"
+      >
+        <AiOutlineTwitter size={20} />
+      </Link>
+      <Link
+        href={`https://twitter.com`}
+        className="text-color-2 border border-color-4 rounded-full w-[50px] h-[50px] grid place-items-center hover:text-color-1 hover:border hover:border-color-1 duration-300"
+      >
+        <AiOutlineGithub size={20} />
+      </Link>
+      <Link
+        href={`https://twitter.com`}
+        className="text-color-2 border border-color-4 rounded-full w-[50px] h-[50px] grid place-items-center hover:text-color-1 hover:border hover:border-color-1 duration-300"
+      >
+        <AiOutlineInstagram size={20} />
+      </Link>
+      <Link
+        href={`https://twitter.com`}
+        className="text-color-2 border border-color-4 rounded-full w-[50px] h-[50px] grid place-items-center hover:text-color-1 hover:border hover:border-color-1 duration-300"
+      >
+        <AiOutlineFacebook size={20} />
+      </Link>
+    </div>
+    <Link
+      href={"/"}
+      className="bg-color-1 px-[58px] py-[10px] text-black border border-transparent rounded-[30px] flex items-center uppercase justify-center gap-3 text-base-regular hover:bg-transparent hover:border hover:border-color-1 hover:text-color-1 duration-300"
+    >
+      <AiOutlineMail size={24} />
+      hire me!
+    </Link>
+  </div>
+);
+export default FixedSideBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,76 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "bg-1": "#999999;",
+        "color-1": "rgb(255, 153, 204)",
+        "color-2": "rgb(153, 153, 153)",
+        "color-3": "rgb(86, 86, 86)",
+        "color-4": "#565656",
+      },
+      fontSize: {
+        "xs-slim": [
+          "12px",
+          {
+            lineHeight: "1.2",
+            fontWeight: "300",
+          },
+        ],
+        "base-regular": [
+          "15px",
+          {
+            lineHeight: "1.7",
+            fontWeight: "400",
+          },
+        ],
+        "sm-regular2": [
+          "14px",
+          {
+            lineHeight: "24px",
+            fontWeight: "400",
+          },
+        ],
+        "sm-regular": [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "400",
+          },
+        ],
+        "2xl-slim": [
+          "24px",
+          {
+            lineHeight: "29px",
+            fontWeight: "300",
+          },
+        ],
+        introduce: [
+          "78px",
+          {
+            lineHeight: "90px",
+            fontWeight: "300",
+          },
+        ],
+        text: [
+          "16px",
+          {
+            lineHeight: "27px",
+            fontWeight: "400",
+          },
+        ],
       },
     },
   },
   plugins: [],
-}
+};
